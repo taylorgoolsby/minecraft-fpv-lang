8eb0b0e038ea78452e413c354e68f7cc4a694438
Taylor Goolsby <taylorgoolsby@users.noreply.github.com>
2020-12-27 19:57:57 -0800

    improve error message

--- test.js
+++ test.js
@@ -17,7 +17,9 @@ test('name format', () => {
       // control
       expect(localeCode.validate(locale)).toBeFalsy()
     } else {
-      expect(localeCode.validate(locale)).toBeTruthy()
+      if (!localeCode.validate(locale)) {
+        throw new Error(filename + ': Your filename is not formatted correctly. Check the README.md for help.')
+      }
     }
   }
 })
@@ -28,7 +30,9 @@ test('file encoding', () => {
     if (filename === 'zz_xx.json5') {
       expect(isutf8(buffer)).toBeFalsy()
     } else {
-      expect(isutf8(buffer)).toBeTruthy()
+      if (!isutf8(buffer)) {
+        throw new Error(filename + ': Your file is not UTF-8 encoded.')
+      }
     }
   }
 })
@@ -40,7 +44,7 @@ test('json5 syntax', () => {
     try {
       JSON5.parse(data)
     } catch (err) {
-      throw new Error(filename + ': ' + err.message)
+      throw new Error(filename + ': Your file is not valid JSON syntax. Reason: ' + err.message)
     }
   }
 })
